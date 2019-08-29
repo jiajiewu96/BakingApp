fe5c535ac5849e218af51ce0e9513054f9bd9c55
jiajie <jiajie.wu96@gmail.com>
2019-08-29 16:24:24 -0700

    Initial fragment setup

--- app/src/main/java/com/example/bakingapp/ui/RecipeFragmentList.java
+++ app/src/main/java/com/example/bakingapp/ui/RecipeFragmentList.java
@@ -1,4 +1,27 @@
 package com.example.bakingapp.ui;
 
-public class RecipeFragmentList {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bakingapp.R;
+
+public class RecipeFragmentList extends Fragment {
+    public RecipeFragmentList(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_recipe_list, container, false);
+        RecyclerView recipeListRecycler = (RecyclerView) rootView.findViewById(R.id.recycler_view_recipe_list);
+        return rootView;
+    }
 }
