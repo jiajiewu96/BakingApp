0775b84ac15ca1278c55239a1dd1a5710bbe59c6
jiajie <jiajie.wu96@gmail.com>
2019-08-29 16:36:42 -0700

    Added adapter for recipe list

---
+++ app/src/main/java/com/example/bakingapp/ui/adapterrs/RecipeListAdapter.java
@@ -0,0 +1,44 @@
+package com.example.bakingapp.ui.adapterrs;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bakingapp.R;
+
+public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.RecipeListViewHolder> {
+
+    public RecipeListAdapter(){
+
+    }
+
+
+    @NonNull
+    @Override
+    public RecipeListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recipe_list, parent, false);
+        return new RecipeListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeListViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class RecipeListViewHolder extends RecyclerView.ViewHolder{
+        TextView recipeTitle;
+        RecipeListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            recipeTitle = (TextView) itemView.findViewById(R.id.tv_recipe_title);
+        }
+    }
+}
