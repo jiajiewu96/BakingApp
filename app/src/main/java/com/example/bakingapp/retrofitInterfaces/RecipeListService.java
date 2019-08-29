3cf4e900ad027ba8cccbbc770e0b470d0a82a9b7
jiajie <jiajie.wu96@gmail.com>
2019-08-29 16:54:37 -0700

    Refactored retrofit interface

--- app/src/main/java/com/example/bakingapp/retrofitInterfaces/RecipeListService.java
+++ app/src/main/java/com/example/bakingapp/retrofitInterfaces/RecipeListService.java
@@ -1,4 +1,4 @@
-package com.example.bakingapp;
+package com.example.bakingapp.retrofitInterfaces;
 
 import com.example.bakingapp.model.RecipeResponse;
 
@@ -6,7 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
-public interface retrofitInterface {
+public interface RecipeListService {
     @GET
     Call<RecipeResponse> getRecipeResponse();
 }
