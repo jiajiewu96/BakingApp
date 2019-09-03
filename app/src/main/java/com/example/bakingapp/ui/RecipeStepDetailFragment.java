6610c5125fe8d55ebe71df044f7dd14fbad83430
jiajie <jiajie.wu96@gmail.com>
2019-09-02 23:59:47 -0700

    Added initial exoplayer code

--- app/src/main/java/com/example/bakingapp/ui/RecipeStepDetailFragment.java
+++ app/src/main/java/com/example/bakingapp/ui/RecipeStepDetailFragment.java
@@ -1,5 +1,7 @@
 package com.example.bakingapp.ui;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +12,43 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.bakingapp.R;
+import com.example.bakingapp.model.Step;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 public class RecipeStepDetailFragment extends Fragment {
 
+    private PlayerView mPlayerView;
+    private SimpleExoPlayer mExoPlayer;
+
+    private Context sContext;
+
+    private Step mStep;
+
+
     public RecipeStepDetailFragment(){
 
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if(sContext == null) {
+            sContext = context;
+        }
     }
 
     @Nullable
@@ -22,6 +56,39 @@ public class RecipeStepDetailFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_recipe_step_detail, container, false);
 
+        mPlayerView = (SimpleExoPlayerView) rootView.findViewById(R.id.player_view);
+        if(isAdded() && sContext != null) {
+            initializePlayer(Uri.parse(mStep.getVideoURL()));
+        }
         return rootView;
+    }
+
+    private void initializePlayer(Uri uri) {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(sContext, trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+            // Prepare the MediaSource.
+            DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(sContext,
+                    Util.getUserAgent(sContext, "Baking App"));
+            MediaSource mediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+            mExoPlayer.prepare(mediaSource);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        releasePlayer();
+    }
+
+    private void releasePlayer(){
+        if(mExoPlayer!=null) {
+            mExoPlayer.stop();
+            mExoPlayer.release();
+            mExoPlayer = null;
+        }
     }
 }
