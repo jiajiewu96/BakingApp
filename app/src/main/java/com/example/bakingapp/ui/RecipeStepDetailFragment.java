cf8eb9cc8d88ba3c1b4714a1a1ca09a93f5e1758
jiajie <jiajie.wu96@gmail.com>
2019-09-03 00:06:41 -0700

    Overrode Exoplayer.Eventlistener

--- app/src/main/java/com/example/bakingapp/ui/RecipeStepDetailFragment.java
+++ app/src/main/java/com/example/bakingapp/ui/RecipeStepDetailFragment.java
@@ -14,13 +14,18 @@ import androidx.fragment.app.Fragment;
 import com.example.bakingapp.R;
 import com.example.bakingapp.model.Step;
 import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 
+import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
@@ -28,7 +33,7 @@ import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
-public class RecipeStepDetailFragment extends Fragment {
+public class RecipeStepDetailFragment extends Fragment implements ExoPlayer.EventListener {
 
     private PlayerView mPlayerView;
     private SimpleExoPlayer mExoPlayer;
@@ -90,5 +95,39 @@ public class RecipeStepDetailFragment extends Fragment {
             mExoPlayer.release();
             mExoPlayer = null;
         }
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, @Nullable Object manifest, int reason) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if(playbackState == ExoPlayer.STATE_READY && playWhenReady){
+            //we are playing
+        }else if(playbackState == ExoPlayer.STATE_READY){
+            //we are paused
+        }
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+
     }
 }
