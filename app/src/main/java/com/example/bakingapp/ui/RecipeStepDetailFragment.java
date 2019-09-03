f636a3374509ef79a0daae911940d427a2889eb1
jiajie <jiajie.wu96@gmail.com>
2019-09-02 22:40:44 -0700

    Added RecipeStepDetailFragment class

---
+++ app/src/main/java/com/example/bakingapp/ui/RecipeStepDetailFragment.java
@@ -0,0 +1,27 @@
+package com.example.bakingapp.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.bakingapp.R;
+
+public class RecipeStepDetailFragment extends Fragment {
+
+    public RecipeStepDetailFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_recipe_step_detail, container, false);
+
+        return rootView;
+    }
+}
