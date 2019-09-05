efc87bcf2485884f8bf45c00dfb002276f70c759
jiajie <jiajie.wu96@gmail.com>
2019-09-05 13:17:30 -0700

    Added checks for screen width

--- app/src/main/java/com/example/bakingapp/ui/RecipeActivity.java
+++ app/src/main/java/com/example/bakingapp/ui/RecipeActivity.java
@@ -22,7 +22,7 @@ import static com.example.bakingapp.utils.Consts.RECIPE_STEP_DETAIL_TRANSACTION_
 import static com.example.bakingapp.utils.Consts.RECIPE_STEP_TRANSACTION_NAME;
 import static com.example.bakingapp.utils.Consts.STEP_KEY;
 
-public class RecipeActivity extends AppCompatActivity implements  RecipeStepListFragment.OnStepClickedListener,
+public class RecipeActivity extends AppCompatActivity implements RecipeStepListFragment.OnStepClickedListener,
         RecipeStepDetailFragment.OnStepChangeClickListener, CommonFragmentInterfaces {
 
     private final FragmentManager mFragmentManager = getSupportFragmentManager();
@@ -35,30 +35,27 @@ public class RecipeActivity extends AppCompatActivity implements  RecipeStepList
         mSw = ((BaseApp) this.getApplication()).screenWidthInDp();
         Recipe recipe = null;
         Bundle bundle = new Bundle();
-        if(savedInstanceState != null)
+        if (savedInstanceState != null)
             recipe = savedInstanceState.getParcelable(RECIPE_KEY);
-        else if (getIntent().getExtras() != null){
+        else if (getIntent().getExtras() != null) {
             recipe = getIntent().getExtras().getParcelable(RECIPE_KEY);
         }
         bundle.putParcelable(RECIPE_KEY, recipe);
         RecipeStepListFragment recipeStepListFragment = new RecipeStepListFragment();
         recipeStepListFragment.setArguments(bundle);
-        if(isTabletScreenWidth()){
+        if (isTabletScreenWidth()) {
             ArrayList<Step> steps = recipe.getSteps();
             bundle.putParcelableArrayList(STEP_KEY, steps);
             bundle.putInt(POSITION_KEY, 0);
 
             RecipeStepDetailFragment recipeStepDetailFragment = new RecipeStepDetailFragment();
             recipeStepDetailFragment.setArguments(bundle);
-            mFragmentManager.beginTransaction()
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                     .add(R.id.step_list_container, recipeStepListFragment)
                     .add(R.id.step_detail_container, recipeStepDetailFragment)
-                    .addToBackStack(RECIPE_STEP_DETAIL_TRANSACTION_NAME)
                     .commit();
-        }
-        else {
-
+        } else {
             mFragmentManager.beginTransaction()
                     .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                     .add(R.id.recipe_list_container, recipeStepListFragment)
@@ -74,22 +71,25 @@ public class RecipeActivity extends AppCompatActivity implements  RecipeStepList
         bundle.putParcelableArrayList(STEP_KEY, steps);
         bundle.putInt(POSITION_KEY, position);
         int containerID = 0;
-        if(isTabletScreenWidth()){
+        if (isTabletScreenWidth()) {
             containerID = R.id.step_detail_container;
-        } else{
+        } else {
             containerID = R.id.recipe_container;
         }
         RecipeStepDetailFragment recipeStepDetailFragment = new RecipeStepDetailFragment();
         recipeStepDetailFragment.setArguments(bundle);
-        mFragmentManager.beginTransaction()
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                .replace(containerID, recipeStepDetailFragment)
-                .addToBackStack(RECIPE_STEP_DETAIL_TRANSACTION_NAME)
-                .commit();
+        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .replace(containerID, recipeStepDetailFragment);
+        if (!isTabletScreenWidth()) {
+            fragmentTransaction.addToBackStack(RECIPE_STEP_DETAIL_TRANSACTION_NAME);
+        }
+
+        fragmentTransaction.commit();
     }
 
     private boolean isTabletScreenWidth() {
-        return mSw>=600;
+        return mSw >= 600;
     }
 
     @Override
@@ -100,9 +100,9 @@ public class RecipeActivity extends AppCompatActivity implements  RecipeStepList
             position--;
         }
         int containerID = 0;
-        if(isTabletScreenWidth()){
+        if (isTabletScreenWidth()) {
             containerID = R.id.step_detail_container;
-        } else{
+        } else {
             containerID = R.id.recipe_container;
         }
         if (position >= 0 && position < steps.size()) {
@@ -111,11 +111,16 @@ public class RecipeActivity extends AppCompatActivity implements  RecipeStepList
             bundle.putInt(POSITION_KEY, position);
             RecipeStepDetailFragment recipeStepDetailFragment = new RecipeStepDetailFragment();
             recipeStepDetailFragment.setArguments(bundle);
-            mFragmentManager.popBackStack();
-            mFragmentManager.beginTransaction()
-                    .replace(containerID, recipeStepDetailFragment)
-                    .addToBackStack(RECIPE_STEP_DETAIL_TRANSACTION_NAME)
-                    .commit();
+            if (!isTabletScreenWidth()) {
+                mFragmentManager.popBackStack();
+            }
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .replace(containerID, recipeStepDetailFragment);
+            if (!isTabletScreenWidth()) {
+                fragmentTransaction.addToBackStack(RECIPE_STEP_DETAIL_TRANSACTION_NAME);
+            }
+            fragmentTransaction.commit();
         }
     }
 
